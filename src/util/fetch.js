2159bb91d648b836ebb9e760c76fa3d9e77e826f
makebanana <530547479@qq.com>
2018-03-13 01:29:34 +0800

    [coding] better hash show

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -59,7 +59,7 @@ export default function fetch (options) {
           {
             type: 'error',
             callback: (action) => {
-              location.href = '/login'
+              location.href = '/'
             }
           }
         )
